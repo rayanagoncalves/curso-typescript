b91ea8ef5af614eba91a62547555bc9e8e636095
rayanagoncalves <rayana.goncalves.cunha@hotmail.com>
2024-08-05 21:25:14 -0300

    feat: tipo boolean

---
+++ modulo-02/10-aula-tipo-boolean/boolean-type.ts
@@ -0,0 +1,15 @@
+// Exemplo 01
+let tarefaConcluida: boolean = true
+let tarefaPendente: boolean = false
+
+console.log(tarefaConcluida)
+console.log(tarefaPendente)
+
+// Exemplo 02
+let concluido: boolean = false
+
+if(!concluido) {
+    console.log('Tarefa foi concluída com sucesso!')
+} else {
+    console.log('Tarefa pendente!')
+}
