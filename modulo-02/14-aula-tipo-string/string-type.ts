08875d7b1d83943035099537746bb4aa026ad7c4
rayanagoncalves <rayana.goncalves.cunha@hotmail.com>
2024-08-06 20:43:47 -0300

    feat: string type

---
+++ modulo-02/14-aula-tipo-string/string-type.ts
@@ -0,0 +1,12 @@
+// Exemplo 1 - String Quotes
+let nomeCompleto = 'Rayana Gonçalves da Cunha'
+console.log(nomeCompleto)
+
+// Exemplo 2 - Double Quotes
+let funcaoEmpresa = "Desenvolvedora Back-end"
+console.log(funcaoEmpresa)
+
+// Exemplo 3 - Back Ticks
+let nomeEmpresa = 'Google'
+let dadosFuncionario = `Seja bem-vinda, ${nomeCompleto}! Você será ${funcaoEmpresa} na empresa ${nomeEmpresa}.`
+console.log(dadosFuncionario)
